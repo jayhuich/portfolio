d2895c722db2d26d7cbadd9bc6be2e7556f0caf9
jay <29498758+jayhuich@users.noreply.github.com>
2023-09-30 22:41:25 +0900

    setup: nextjs, eslint, prettier, husky, lint-staged

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between p-24">
+      <h1 className="text-xl">hello world!</h1>
+    </main>
+  );
+}
