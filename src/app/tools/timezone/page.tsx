423cdcbfdb2b248810a4b2371db9d604ffa18d96
jay <29498758+jayhuich@users.noreply.github.com>
2023-10-07 19:45:28 +0900

    feature: added react useState

--- src/app/tools/timezone/page.tsx
+++ src/app/tools/timezone/page.tsx
@@ -4,6 +4,8 @@ import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import * as React from "react";
 
+import { Button } from "@mui/material";
+
 type Timezone = {
   tz: string;
   city?: string;
@@ -21,6 +23,11 @@ export default function TimezonePage() {
     { tz: dayjs.tz.guess() }
   ];
 
+  const [time, setTime] = React.useState(dayjs());
+  const increment = () => {
+    setTime((time) => time.add(1, "h"));
+  };
+
   return (
     <main className="flex flex-col items-center justify-between p-10 gap-10">
       <h1 className="text-3xl font-bold">Timezone Converter</h1>
@@ -30,11 +37,15 @@ export default function TimezonePage() {
             <div key={i} className="my-4">
               <p>City: {e.city || e.tz.split("/").pop()?.replaceAll("_", " ")}</p>
               <p>Timezone: {e.tz}</p>
-              <p>Time: {dayjs().tz(e.tz).format("(ddd), MMM D, YYYY HH:mm A")}</p>
+              <p>Time: {time.tz(e.tz).format("(ddd), MMM D, YYYY HH:mm A")}</p>
             </div>
           );
         })}
       </div>
+
+      <Button variant="outlined" onClick={increment}>
+        Add 1 Hour
+      </Button>
     </main>
   );
 }
